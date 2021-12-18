0e14a64fabcf30ddb7099c087729267e2a7b3d6b
Milos Andrejic <milosandrejic1993@gmail.com>
2021-12-18 13:27:33 +0100

    components

--- packages/common/webpack.config.js
+++ packages/common/webpack.config.js
@@ -9,7 +9,8 @@ module.exports = (env, argv) => {
         devtool: (isDevelopment ? "source-map" : false),
         entry: {
             "widgets": "./lib/widgets/index.js",
-            "themes": "./lib/themes/index.js"
+            "themes": "./lib/themes/index.js",
+            "components": "./lib/components/index.js"
         },
         watch: isDevelopment,
         output: {
